ab67664f49c138279b5176a40e13e70349ae75d6
7amza Taouil <hamzataouil00@gmail.com>
2023-03-27 15:49:11 +0000

    Java OOP learning (Encapsulation) [improved]

--- src/Employee.java
+++ src/Employee.java
@@ -1,9 +1,26 @@
 public class Employee {
-    public int baseSalary;
-
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculatedWage(int extraHours){
-        return baseSalary + (hourlyRate * extraHours);
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+    public void setBaseSalary( int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary can't be 0 or negative!");
+        this.baseSalary = baseSalary;
+    }
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate can't be 0 or negative!");
+        this.hourlyRate = hourlyRate;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
-
-        int wage = employee.calculatedWage(30);
+        employee.setBaseSalary(50000);
+        employee.getBaseSalary();
+        employee.getHourlyRate();
+        employee.setHourlyRate(20);
+        int wage = employee.calculatedWage( 20);
         System.out.println(wage);
 
     }
